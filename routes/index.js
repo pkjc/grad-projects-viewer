ce3f457058fdcc7e80229b50ba7e0de591c5d467
pkj <mepankaj@live.in>
2018-03-21 17:39:57 -0400

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'CSI5510 Assignment 3' });
 });
 
 module.exports = router;
