ed2416587ca1a7746307cfdcfabe70510e313691
Aditi Patil <aditipatil@oakland.edu>
2018-03-23 15:30:23 -0400

    app.js route path

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var projects = require('./routes/project');
@@ -40,7 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
+app.use('/', index);
 app.use('/', routes);
 app.use('/users', users);
 app.use('/user', users);
