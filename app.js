e94cc0a181bc20c5fe3766386b6e129cdbcf8638
Rashmi Pethe <rashmipethe@oakland.edu>
2018-03-22 22:48:18 -0400

    Modified to add modal and server end code to display project links

--- app.js
+++ app.js
@@ -49,8 +49,4 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-app.listen(3000, function(){
-	console.log('Server is listening at port 3000');
-});
-
 module.exports = app;
--- routes/project.js
+++ routes/project.js
@@ -5,7 +5,7 @@ var mongoClient = require('mongodb').MongoClient, assert = require('assert');
 var url = 'mongodb://localhost:27017/gradprojects';
 
 var projectArray;
-
+var user;
 mongoClient.connect(url, function(err, client){
 	if(err){
 		return console.dir(err);
@@ -13,18 +13,22 @@ mongoClient.connect(url, function(err, client){
 
 	console.log("Connected Successfully to mongo server");
 	var db = client.db('gradprojects');
-	var query= {user:"rashmipethe"}
+	var query= {user:"rashmipethe"};
 	db.collection('projects').find(query).toArray(function(err, result){
 		if(err){
 			throw err;
 		}
+
+		projectArray = result[0].project;
 		var intLength = result.length;
 		for (var i= 0; i < intLength ; i++) {
 			var projects = result[i];
 			console.log(projects.user);
+			user = projects.user;
 			var pLength = projects.project.length;
-			projectArray = projects.project;
+			
 			for (var j = 0; j < pLength; j++) {
+				
 				console.log(projects.project[j].pname);
 			}
 			
@@ -35,9 +39,23 @@ mongoClient.connect(url, function(err, client){
 	client.close();
 });
 
+router.get('/projectlist', function(req, res) {
+	console.log("req received");
+	console.log(req.query.name);
+	mongoClient.connect(url, function(err, client){
+		if(err){
+			return console.dir(err);
+		}
+		var db = client.db('gradprojects');
+		var query= {user:"rashmipethe"};
+		console.log("connected again");
+	});
+	client.close();
+	res.send("Ajax call success");
+})
 
 router.get('/', function(req, res){
-	res.render('project');
+	res.render('project', {user:user, list: projectArray});
 });
 
 module.exports = router;
