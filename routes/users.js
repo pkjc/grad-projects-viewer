c6b5cb3813d4a7aea983e5d5f4b804637f1d714c
pkj <mepankaj@live.in>
2018-03-24 11:52:10 -0400

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+/* GET users listing. */
+router.get('/user/:id', function(req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
